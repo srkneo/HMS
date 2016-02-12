14f9734ec2994d0a1d6a537746754b7005a4ba1e
srkneo <yadavmaheshing@gmail.com>
2016-02-12 15:51:23 +0530

    Max

---
+++ HMS/Controllers/SuperAdminController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace HMS.Controllers
+{
+    public class SuperAdminController : Controller
+    {
+        //
+        // GET: /SuperAdmin/
+        public ActionResult Index()
+        {
+            return View();
+        }
+	}
+}
