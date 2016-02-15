fcca82d17530c450a7f0a9a7e8c6d0ffd729ecd6
srkneo <yadavmaheshing@gmail.com>
2016-02-15 09:28:20 +0530

    Max

---
+++ HMS/Models/User.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace HMS.Models
+{
+    public partial class User
+    {
+        class LoginUserData
+        {
+            [Required(ErrorMessage = "Employee Id is Required")]
+            public string UserName { get; set; }
+
+            [Required(ErrorMessage = "Password is Required")]
+            public string Password { get; set; }
+        }
+    }
+}
