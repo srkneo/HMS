14574a11389ea96a7116d304dfa50e3bb98720a7
srkneo <yadavmaheshing@gmail.com>
2016-03-05 10:11:54 +0530

    Max

---
+++ HMS/Infrastructure/CustomAuthorizationAttribute.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Security;
+using System.Web.Mvc;
+
+namespace HMS.Infrastructure
+{
+    public class CustomAuthorizationAttribute : AuthorizeAttribute
+    {
+        public string LoginPage { get; set; }
+        public string Role { get; set; }
+
+        public override void OnAuthorization(AuthorizationContext filterContext)
+        {
+
+            //Send the Return url to login page
+
+            LoginPage += "?ReturnUrl=" + filterContext.HttpContext.Request.RawUrl;
+
+            if(!filterContext.HttpContext.User.Identity.IsAuthenticated)
+            {
+                filterContext.HttpContext.Response.Redirect(LoginPage);
+            }
+            else
+            {
+                if(Role != null)
+                {
+                    if(!filterContext.HttpContext.User.IsInRole(Role))
+                    {
+                        filterContext.HttpContext.Response.Redirect(LoginPage);
+                    }
+                }
+            }
+
+            base.OnAuthorization(filterContext);
+        }
+
+    }
+}
